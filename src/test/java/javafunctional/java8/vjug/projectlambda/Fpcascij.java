0106fdba1c0ec5e66c2a06c3c2427bee79c49477
Demian Neidetcher <demian0311@gmail.com>
2014-02-27 23:26:01 -0600

    VJUG

---
+++ src/test/java/javafunctional/java8/vjug/projectlambda/Fpcascij.java
@@ -0,0 +1,52 @@
+package javafunctional.java8.vjug.projectlambda;
+
+import jtrade.Stock;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Functional
+ * Programming
+ * Constructs
+ * And
+ * Simpler
+ * Concurrency
+ * In Java
+ */
+public class Fpcascij {
+    /**
+     * - client controls iteration
+     * - inherently serial
+     * - not thread safe
+     */
+    @Test public void externalIteraion(){
+        Double highestValue = Double.MIN_VALUE;
+
+        for(Stock s: Stock.portfolio){
+            if(s.getQuantity() > 50){
+                if(s.getValue() > highestValue){
+                    highestValue = s.getValue();
+                }
+            }
+        }
+
+        assertEquals(9228.28, highestValue, 1);
+    }
+
+    /**
+     * 
+     */
+    @Test public void internalWithLambdas(){
+        OptionalDouble highestValue = Stock.portfolio.stream()
+                .filter(s -> s.getQuantity() > 50)
+                .mapToDouble(s -> s.getValue())
+                .max();
+
+        assertEquals(9228.28, highestValue.getAsDouble(), 1);
+    }
+}
