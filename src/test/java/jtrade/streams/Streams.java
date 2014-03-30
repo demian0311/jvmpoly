28c65039eb67e2b0c242caab58a2de6dbe61f8fb
Demian Neidetcher <demian0311@gmail.com>
2014-03-30 17:22:43 -0500

    lots of examples for streams

--- src/test/java/jtrade/streams/Streams.java
+++ src/test/java/jtrade/streams/Streams.java
@@ -4,9 +4,7 @@ import jtrade.Stock;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -76,5 +74,91 @@ public class Streams {
                 .collect(Collectors.toList());   // terminal
         assertEquals(Arrays.asList(3, 3, 3), stringList);
     }
+
+    @Test public void mapToInt(){
+        int lengthOfStrings = Arrays.asList("FOO", "BAR", "BAZ")
+                .stream()                 // source
+                .mapToInt(String::length) // intermediate
+                .sum();                   // terminal
+        assertEquals(9, lengthOfStrings);
+    }
+
+    @Test public void flatMap(){
+        List<List<String>> twoDimensionalList = Arrays.asList(
+                Arrays.asList("a0", "a1", "a2"),
+                Arrays.asList("b0", "b1", "b2"),
+                Arrays.asList("c0", "c1", "c2"));
+
+        List<String> actual = twoDimensionalList
+                .stream()                      // source
+                .flatMap(l -> l.stream())      // intermediate
+                .collect(Collectors.toList()); // terminal
+
+        List<String> expected =
+                Arrays.asList(
+                        "a0", "a1", "a2",
+                        "b0", "b1", "b2",
+                        "c0", "c1", "c2");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test public void sorted(){
+        List<String> actual = Arrays.asList("FOO", "BAR", "BAZ")
+                .stream()                         // source
+                .sorted((a, b) -> a.compareTo(b)) // intermediate
+                .collect(Collectors.toList());    // terminal
+
+        List<String> expected = Arrays.asList("BAR", "BAZ", "FOO");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test public void forEach(){
+        Arrays.asList("FOO", "BAR", "BAZ")
+                .stream()
+                .forEach(System.out::println);
+    }
+
+    @Test public void reduce(){
+        Integer sum = Arrays.asList(1, 2, 3, 5, 8, 13)
+                .stream()                 // source
+                .filter(s -> s % 2 == 0)  // intermediate
+                .reduce(0, Integer::sum); // terminal
+        assertEquals(new Integer(10), sum);
+    }
+
+    @Test public void collect(){
+        List<Integer> evenIntegers = Arrays.asList(1, 2, 3, 5, 8, 13)
+                .stream()                      // source
+                .filter(s -> s % 2 == 0)       // intermediate
+                .collect(Collectors.toList()); // terminal
+
+        List<Integer> expected = Arrays.asList(2, 8);
+        assertEquals(expected, evenIntegers);
+    }
+
+    @Test public void collectMultiArgument(){
+        List<Integer> evenIntegers = Arrays.asList(1, 2, 3, 5, 8, 13)
+                .stream()                      // source
+                .filter(s -> s % 2 == 0)       // intermediate
+                .collect(                      // terminal
+                        ArrayList::new,
+                        ArrayList::add,
+                        ArrayList::addAll);
+
+        List<Integer> expected = Arrays.asList(2, 8);
+        assertEquals(expected, evenIntegers);
+    }
+
+    @Test public void collectGroupingBy(){
+        Map<Boolean, List<Integer>> evensOdds = Arrays.asList(1, 2, 3, 5, 8, 13)
+                .stream()
+                .collect(Collectors.groupingBy(i -> i % 2 == 0));
+
+        assertEquals(Arrays.asList(1, 3, 5, 13), evensOdds.get(false));
+        assertEquals(Arrays.asList(2, 8), evensOdds.get(true));
+    }
+
 
 }
